5660090d52bf45a2b17ec235c5e2d566521276a2
Luis Alberto Villegas Valenzuela <lualviva97@gmail.com>
2022-04-24 21:16:09 -0500

    GET agregado

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.get("/v1/explorers", (req, res) => {
+  console.log(`Api Explorers GET ALL requests ${new Date()}`);
+  const explorer1 = { id: 1, name: "Villegas1" };
+  const explorer2 = { id: 2, name: "Villegas2" };
+  const explorer3 = { id: 3, name: "Villegas3" };
+  const explorer4 = { id: 4, name: "Villegas4" };
+  const explorers = [explorer1, explorer2, explorer3, explorer4];
+  res.status(200).json(explorers);
+});
