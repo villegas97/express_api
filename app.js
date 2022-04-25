7b4aed2d84b700df7e2defe72c9798c6ed84e662
Luis Alberto Villegas Valenzuela <lualviva97@gmail.com>
2022-04-24 21:29:43 -0500

    Agregado método DELETE

--- app.js
+++ app.js
@@ -38,3 +38,10 @@ app.put("/v1/explorers/:id", (req, res) => {
   const requestBody = req.body;
   res.status(200).json({ message: "Updated!" });
 });
+
+app.delete("/v1/explorers/:id", (req, res) => {
+  console.log(`Api Explorers DELETE request ${new Date()}`);
+  console.log(`Delete explorer with id ${req.params.id}`);
+  const requestBody = req.body;
+  res.status(200).json({ message: "Deleted" });
+});
